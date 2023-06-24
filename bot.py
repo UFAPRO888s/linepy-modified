42eb64e8c65385d4c58ad7f1381192661da32a6f
beyourbabe60 <beyourbabe60@gmail.com>
2023-06-24 09:18:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,32 +13,32 @@ line.log("Timeline Token : " + str(line.tl.channelAccessToken))
 # Initialize OEPoll with LINE instance
 oepoll = OEPoll(line)
 
-def get_hashed_text_with_secret_key(secret_key: str, payload: str, method = hashlib.sha256) -> str:
-    return hmac.new(secret_key.encode("utf-8"), payload.encode("utf-8"), method).hexdigest()
+# def get_hashed_text_with_secret_key(secret_key: str, payload: str, method = hashlib.sha256) -> str:
+#     return hmac.new(secret_key.encode("utf-8"), payload.encode("utf-8"), method).hexdigest()
 
-def get_encrypt_data(payload: str, secret_key: str, vector: str):
-    b64_payload = base64.b64encode(payload.encode("utf-8"))
+# def get_encrypt_data(payload: str, secret_key: str, vector: str):
+#     b64_payload = base64.b64encode(payload.encode("utf-8"))
 
-    if len(b64_payload) % 16 != 0:
-        for _ in range(16 - (len(b64_payload) % 16)):
-            b64_payload += b"_"
+#     if len(b64_payload) % 16 != 0:
+#         for _ in range(16 - (len(b64_payload) % 16)):
+#             b64_payload += b"_"
 
-    secret_key = hashlib.sha256(secret_key.encode("utf-8")).digest()
-    vector = hashlib.md5(vector.encode("utf-8")).digest()
+#     secret_key = hashlib.sha256(secret_key.encode("utf-8")).digest()
+#     vector = hashlib.md5(vector.encode("utf-8")).digest()
 
-    aes = AES.new(secret_key, AES.MODE_CBC, vector)
-    cipher_data = aes.encrypt(b64_payload)
-    return base64.b64encode(cipher_data).decode("utf-8")
+#     aes = AES.new(secret_key, AES.MODE_CBC, vector)
+#     cipher_data = aes.encrypt(b64_payload)
+#     return base64.b64encode(cipher_data).decode("utf-8")
 
-def get_decrypt_data(b64_cipher: str, secret_key: str, vector: str):
-    cipher_data = base64.b64decode(b64_cipher.encode("utf-8"))
+# def get_decrypt_data(b64_cipher: str, secret_key: str, vector: str):
+#     cipher_data = base64.b64decode(b64_cipher.encode("utf-8"))
 
-    secret_key = hashlib.sha256(secret_key.encode("utf-8")).digest()
-    vector = hashlib.md5(vector.encode("utf-8")).digest()
-    aes = AES.new(secret_key, AES.MODE_CBC, vector)
+#     secret_key = hashlib.sha256(secret_key.encode("utf-8")).digest()
+#     vector = hashlib.md5(vector.encode("utf-8")).digest()
+#     aes = AES.new(secret_key, AES.MODE_CBC, vector)
 
-    b64_payload = aes.decrypt(cipher_data)
-    return base64.b64decode(b64_payload.partition(b"_")[0]).decode("utf-8")
+#     b64_payload = aes.decrypt(cipher_data)
+#     return base64.b64decode(b64_payload.partition(b"_")[0]).decode("utf-8")
 
 # secret_key = "encrypted_mid_key"
 # mid = "u35db685708155a4f03cce9c8e1799c41"
@@ -51,10 +51,10 @@ def get_decrypt_data(b64_cipher: str, secret_key: str, vector: str):
 
 # token = get_decrypt_data(enc_token, mid, primary_key)
 # print("token:", token)
-mid = "c5b8b1cf34fcd70f34b66082b7712fa"
-token = "szXvOjvlmIEuPfRHR8u8joHyQRsnM2fGd7YUVWoK5sxA4NhyC4JsroAnHtYMl57//8kNaNbUVGomvs9kYt6gCY8BG+xDMoIuqvMFeduraoyR0tG9+oUSxWLZ2wQI0YsbRCzPHGkelFrXYjbbF6XEBSNcGBe/QgDJJb67158ijjFwZil231bU8/I8EWoHOWsV"
-secret_key = "encrypted_mid_key"
-primary_key = get_hashed_text_with_secret_key(secret_key, mid)
+# mid = "c5b8b1cf34fcd70f34b66082b7712fa"
+# token = "szXvOjvlmIEuPfRHR8u8joHyQRsnM2fGd7YUVWoK5sxA4NhyC4JsroAnHtYMl57//8kNaNbUVGomvs9kYt6gCY8BG+xDMoIuqvMFeduraoyR0tG9+oUSxWLZ2wQI0YsbRCzPHGkelFrXYjbbF6XEBSNcGBe/QgDJJb67158ijjFwZil231bU8/I8EWoHOWsV"
+# secret_key = "encrypted_mid_key"
+# primary_key = get_hashed_text_with_secret_key(secret_key, mid)
 # Receive messages from OEPoll
 def RECEIVE_MESSAGE(op):
 
@@ -64,14 +64,11 @@ def RECEIVE_MESSAGE(op):
     receiver = msg.to
     sender = msg._from
     chunks = msg.chunks
-    print(msg)
-    
     try:
-        chunk = get_encrypt_data(chunks,mid,primary_key)
-        print(chunk)
+        text.lower()
     except:
         pass
-    
+    print(chunks,text)
     try:
         # Check content only text message
         if msg.contentType == 0:
@@ -82,10 +79,7 @@ def RECEIVE_MESSAGE(op):
                 # Get sender contact
                 contact = line.getContact(sender)
                 # Command list
-                try:
-                    text.lower()
-                except:
-                    pass
+                
                 if text == 'hi':
                     line.log('[%s] %s' % (contact.displayName, text))
                     line.sendMessage(receiver, 'Hi too! How are you?')
